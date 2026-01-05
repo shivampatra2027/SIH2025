bc8f8131f7d9e6b3d4b57c51d9ca6155659ef6be
shivam patra <spworld30@gmail.com>
2026-01-05 22:21:39 +0530

    fix:axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 // Use env variable in production, localhost in dev
 const BASE_URL =
-  import.meta.env.MODE = import.meta.env.VITE_API_URL; // points to backend domain
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : import.meta.env.VITE_API_URL; // points to backend domain
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
